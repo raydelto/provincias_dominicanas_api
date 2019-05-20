7768991af1d06bc0376f03db53793080906fce86
Raydelto Hernandez [QPS] <raydelto.hernandez@qps.nl>
2019-05-20 19:41:01 -0300

    Corrigiendo typo en archivo de configuracion

--- includes/config.php
+++ includes/config.php
@@ -2,4 +2,4 @@
 $config["DB_HOST"] = "localhost";
 $config["DB_USER"] = "USUARIO DE LA BASE DE DATOS";
 $config["DB_PASSWORD"] = "CONTRASEÑA DE LA BASE DE DATOS";
-$config["DB_NAME"] = "NOMBRE DE LA BAE DE DATOS";
+$config["DB_NAME"] = "NOMBRE DE LA BASE DE DATOS";
